1ecf600a997d58d28747356f3ca1af726f9878ad
lizhitao <lizhitao@BJJR-LIZHITAO.360buyAD.local>
2015-04-08 20:23:13 +0800

    删除无用代码

--- network/src/org/sagacity/network/nio/ByteBufferDemo2.java
+++ network/src/org/sagacity/network/nio/ByteBufferDemo2.java
@@ -18,6 +18,18 @@ public class ByteBufferDemo2 {
 		ByteBuffer buffer = ByteBuffer.allocate(10);
 		byte[] bytes = new byte[]{1,2,3,4,5,6,7,8};
 		buffer.put(bytes);
+		buffer.flip();
+		System.out.println(buffer);
+//		System.out.println(buffer.limit());
+//		buffer.limit(buffer.position());
+		buffer.position(2);
+		buffer.compact();
+		buffer.flip();
+		System.out.println(buffer);
+		
+		while(buffer.hasRemaining()){
+			System.out.print(buffer.get());
+		}
 		
 	}
 
--- network/src/org/sagacity/network/socket/server/nonblocking/NioNonBlockingEchoServer.java
+++ network/src/org/sagacity/network/socket/server/nonblocking/NioNonBlockingEchoServer.java
@@ -127,7 +127,6 @@ public class NioNonBlockingEchoServer {
 		SocketChannel socketChannel = (SocketChannel) key
 				.channel();
 		ByteBuffer readBuffer = ByteBuffer.allocate(32);
-		System.out.println(socketChannel.isOpen());
 		socketChannel.read(readBuffer);
 		readBuffer.flip();
 		buffer.limit(buffer.capacity());
@@ -145,54 +144,6 @@ public class NioNonBlockingEchoServer {
 
 	public String echoStr(String msg) {
 		return "echo: " + msg;
-	}
-
-	/**
-	 * 使用channel读取缓冲区数据
-	 * 
-	 * @param socketChannel
-	 * @return
-	 * @throws IOException
-	 */
-	public String readLine(SocketChannel socketChannel) throws IOException {
-		// 存放所有读到的数据，假设一行字符串对应的字节序列的长度小于1024
-		ByteBuffer buffer = ByteBuffer.allocate(1024);
-		// 存放一次读到的数据，一次只读一个字节
-		ByteBuffer tempBuffer = ByteBuffer.allocate(1);
-		boolean isLine = false;// 表示是否读到了一行字符串
-		boolean isEnd = false;// 表示是否到达了输入流的末尾
-		String data = null;
-		while (!isLine && !isEnd) {
-			tempBuffer.clear();// 清空缓冲区
-			// 在阻塞模式下，只有读到了一个字节或者读到输入流末尾才返回
-			// 在非阻塞模式下有可能返回0
-			int n = socketChannel.read(tempBuffer);
-			if (n == -1) {// 读到了输入流的末尾
-				isEnd = true;
-				break;
-			}
-			if (n == 0) {
-				continue;
-			}
-
-			tempBuffer.flip();// 把极限设为位置，把位置设为0
-			buffer.put(tempBuffer);// 把tempBuffer中的数据复制到buffer中
-			buffer.flip();
-			Charset charset = Charset.defaultCharset();
-			CharBuffer charBuffer = charset.decode(buffer);// 解码
-			data = charBuffer.toString();
-			if (data.indexOf("\r\n") != -1) {// 读到了一行数据
-				isLine = true;
-				data = data.substring(0, data.indexOf("\r\n"));
-				break;
-			}
-			buffer.position(buffer.limit());// 把位置设为极限，为下次读数据做准备
-			buffer.limit(buffer.capacity());// 把极限设为容量，为下次读数据做准备
-		}
-
-		// 如果读取到一行数据就返回这行数据不包括\r\n
-		// 如果读取到输入流末尾就返回null
-		return data;
 	}
 
 	/**
